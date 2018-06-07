586db2a1a60cf0805a04e2728aa53c99f7ca3520
Wyns Stéphane <stewyns.pro@gmail.com>
2018-06-07 13:37:45 +0200

    Version 0.1

--- views/footer.php
+++ views/footer.php
@@ -1,3 +1,15 @@
 </div>
 
 <!-- footer en dessous -->
+
+<footer>
+    <div class="instagram">
+        <i class="fab fa-instagram fa-5x"></i>
+    </div>
+    <div class="facebook">
+        <i class="fab fa-facebook-f fa-5x"></i>
+    </div>
+    <div class="pinterest">
+        <i class="fab fa-pinterest fa-5x"></i>
+    </div>
+</footer>
