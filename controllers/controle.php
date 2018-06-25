d6d8e3858ed2f085a0b29ac8a73928bcaceb1372
leersmathieu <leers.mathieu@gmail.com>
2018-06-25 09:10:47 +0200

    header/router

--- controllers/controle.php
+++ controllers/controle.php
@@ -1,13 +1,13 @@
 <?php 
     if ($action == 'home'){
 
-        require "views/nav.php";
-        require "views/burgerMenu.php";
-        require "views/header.php";
-        require "views/content.php";
-        require "views/moreInfo.php";
-        require "views/contact.php";
-        require "views/footer.php";
+        require("views/nav.php");
+        require("views/burgerMenu.php");
+        require("views/header.php");
+        require("views/content.php");
+        require("views/moreInfo.php");
+        require("views/contact.php");
+        require("views/footer.php");
 
     }
 
--- views/header.php
+++ views/header.php
@@ -1,6 +1,8 @@
 <div class="page">
 
     <header>
-        <?php require "assets/img/logo_ritournelle.svg" ?>
-        <p><strong>* De mains en mains *</strong></p>
+        <img src="views/assets/img/logo_ritournelle.svg" alt="ritournelle"/>
+        <p>
+            <strong>* De mains en mains *</strong>
+        </p>
     </header>
