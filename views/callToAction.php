0094d927dcd136c0feaaeabab421438717cfc172
leersmathieu <leers.mathieu@gmail.com>
2018-06-29 14:43:38 +0200

    call to action

---
+++ views/callToAction.php
@@ -0,0 +1,8 @@
+<section class="callToAction">
+    <div class="callFacebook">
+        <a href="https://www.facebook.com/laritournelleboutique/" target="_blank">
+            <button class="btn">Visite notre Facebook !</button>
+        
+        </a>
+    </div>
+</section>
